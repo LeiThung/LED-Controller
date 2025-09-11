4dffbeb0aabc4941ea54f149ad601e088c61eadf
fas <Fabius.Schurig@communardo.de>
2025-09-11 09:52:11 +0200

    Changed App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
+import ColorPicker from '@rc-component/color-picker';
+import '@rc-component/color-picker/assets/index.css';
 
 function App() {
 
@@ -32,8 +33,22 @@ function App() {
             .then((res) => res.json())
     }
 
+    const customLed = (value: string) => {
+        setTimeout(() => {
+            fetch("http://192.168.88.103:5000/api/color-picker", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ color: value }),
+            })
+                .then((res) => res.json());
+        }, 1000); // 500ms Pause
+    };
+
   return (
     <div className="App">
+        <ColorPicker onChange={(value) => {customLed(value.toRgbString())}}/>
         <button onClick={ledOff}>Off</button>
         <button onClick={ledRed}>Rot</button>
         <button onClick={ledBlue}>Blau</button>
